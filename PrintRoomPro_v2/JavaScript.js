f2e5394ec8c64fe28494fabf28d318522a46a661
Jedidiah May <portlandtn25@hotmail.com>
2018-09-27 16:10:15 -0500

    Updated froms, added Java, corrected Routing Sheet

--- PrintRoomPro_v2/JavaScript.js
+++ PrintRoomPro_v2/JavaScript.js
@@ -1,9 +1,80 @@
-﻿var CSR = "Jody Woodall";
-var Bldr = "Jones Construction";
+﻿//These variables will populate from the database based on job # input on
+//the home screen.
+var CSR = "Jedidiah May";
+var builder = "Jones Construction";
+var state = "TN";
+
+var jobNumber = "K12A0205A";
+var issueNumber = 1;
+var revisionCode = 4;
+var currentDate = "9/25/2018";
+var omittedFabs = "R1, R18";
+var group1ShipperPages = "1-2, 9";
+var group1Fabs = "R2-R17, R21-R22";
+var group2_3ShipperPages = "3-8";
+var group2_3Fabs = "R19-R20";
+var group4ShipperPages = "9-11, 15";
+var group5ShipperPages = "12-14";
+var group6ShipperPages = "16-21";
+var group6Fabs = "R23-R25";
+var group7ShipperPages = "22-30";
+var group8ShipperPages = "31-32";
+var panelCutlistPages = "33";
+var stockCoilSummary = "34";
 
 $(document).ready(function () {
-    $("#CSR_name").text(CSR);
-    $('#Bldr_name').text(Bldr);
+    $('.CSR_name').text(CSR);
+    $('.bldrName').text(builder);
+    $('#state_initials').text(state);
+    $('#jobNumberBox').text(jobNumber);
+    $('#bldrBox').text(builder);
+    $('#issueBox').text(issueNumber);
+    $('#currentDate').text(currentDate);
+    $('#omittedFabs').text(omittedFabs);
+    $('#group1ShipperPages').text(group1ShipperPages);
+    $('#group1Fabs').text(group1Fabs);
+    $('#group2_3ShipperPages').text(group2_3ShipperPages);
+    $('#group2_3Fabs').text(group2_3Fabs);
+    $('#group4ShipperPages').text(group4ShipperPages);
+    $('#group5ShipperPages').text(group5ShipperPages);
+    $('#group6ShipperPages').text(group6ShipperPages);
+    $('#group6Fabs').text(group6Fabs);
+    $('#group7ShipperPages').text(group7ShipperPages);
+    $('#group8ShipperPages').text(group8ShipperPages);
+    $('#panelCutlistPages').text(panelCutlistPages);
+    $('#stockCoilSummary').text(stockCoilSummary);
+
+    switch (revisionCode) {
+        case "0":
+            break;
+        case 1:
+            $('#revcode1').css("background-color", "darkgray");
+            break;
+        case 2:
+            $('#revcode2').css("background-color", "darkgray");
+            break;
+        case 3:
+            $('#revcode3').css("background-color", "darkgray");
+            break;
+        case 4:
+            $('#revcode4').css("background-color", "darkgray");
+            break;
+        case 5:
+            $('#revcode5').css("background-color", "darkgray");
+            break;
+        case 6:
+            $('#revcode6').css("background-color", "darkgray");
+            break;
+        case 7:
+            $('#revcode7').css("background-color", "darkgray");
+            break;
+        case 8:
+            $('#revcode8').css("background-color", "darkgray");
+            break;
+        case 9:
+            $('#revcode9').css("background-color", "darkgray");
+            break;
+    }
 });
 
 function show_hide_manual_checkboxes() {
@@ -44,4 +115,27 @@ function show_hide_manual_checkboxes() {
         checkBox_AWIP.style.display = "none";
         label_AWIP.style.display = "none";
     }
+}
+
+function show_hide_StateSeal() {
+    var checkBox_StateSeal = document.getElementById("chk_seals");
+
+    var label_StateSeal = document.getElementById("state");
+
+    if (checkBox_StateSeal.checked == true) {
+        label_StateSeal.style.display = "inline";
+    }
+    else {
+        label_StateSeal.style.display = "none";
+    }
+}
+
+function populateRoutingSheetData() {
+
+    $("#jobNumberBox").text(jobNumber);
+    $("#bldrBox").text(Bldr);
+}
+
+function submit_action() {
+    alert("yo, sup?")
 }
